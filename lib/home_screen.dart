0363745c448b1d743196fde992fd530d059f6d53
rn-byte <ramthakur977+github@gmail.com>
2024-11-30 21:53:58 +0545

    homePage design completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,14 +7,20 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Interest Calculator'),
-        backgroundColor: Colors.purple[100],
+        title: const Text(
+          'Interest Calculator',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromARGB(255, 64, 3, 75),
         elevation: 0,
         centerTitle: true,
         actions: [
           IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.settings),
+            icon: const Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
           ),
         ],
       ),
@@ -29,89 +35,83 @@ class HomeScreen extends StatelessWidget {
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
               SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    DropdownButton<String>(
-                      hint: const Text('Year'),
-                      items: List.generate(100, (index) {
-                        return DropdownMenuItem(
-                          value: (2000 + index).toString(),
-                          child: Text((2030 + index).toString()),
-                        );
-                      }),
-                      onChanged: (value) {},
-                    ),
-                    DropdownButton<String>(
-                      hint: const Text('Month'),
-                      items: List.generate(12, (index) {
-                        return DropdownMenuItem(
-                          value: (index + 1).toString(),
-                          child: Text((index + 1).toString()),
-                        );
-                      }),
-                      onChanged: (value) {},
-                    ),
-                    DropdownButton<String>(
-                      hint: const Text('Day'),
-                      items: List.generate(32, (index) {
-                        return DropdownMenuItem(
-                          value: (index + 1).toString(),
-                          child: Text((index + 1).toString()),
-                        );
-                      }),
-                      onChanged: (value) {},
-                    ),
-                  ],
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  DropdownButton<String>(
+                    hint: const Text('Year'),
+                    items: List.generate(100, (index) {
+                      return DropdownMenuItem(
+                        value: (2000 + index).toString(),
+                        child: Text((2030 + index).toString()),
+                      );
+                    }),
+                    onChanged: (value) {},
+                  ),
+                  DropdownButton<String>(
+                    hint: const Text('Month'),
+                    items: List.generate(12, (index) {
+                      return DropdownMenuItem(
+                        value: (index + 1).toString(),
+                        child: Text((index + 1).toString()),
+                      );
+                    }),
+                    onChanged: (value) {},
+                  ),
+                  DropdownButton<String>(
+                    hint: const Text('Day'),
+                    items: List.generate(32, (index) {
+                      return DropdownMenuItem(
+                        value: (index + 1).toString(),
+                        child: Text((index + 1).toString()),
+                      );
+                    }),
+                    onChanged: (value) {},
+                  ),
+                ],
               ),
               SizedBox(height: MediaQuery.of(context).size.height * 0.02),
               const Text(
                 'Enter End Date',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    DropdownButton<String>(
-                      hint: const Text('Year'),
-                      items: List.generate(100, (index) {
-                        return DropdownMenuItem(
-                          value: (2030 + index).toString(),
-                          child: Text((2000 + index).toString()),
-                        );
-                      }),
-                      onChanged: (value) {},
-                    ),
-                    DropdownButton<String>(
-                      hint: const Text('Month'),
-                      items: List.generate(12, (index) {
-                        return DropdownMenuItem(
-                          value: (index + 1).toString(),
-                          child: Text((index + 1).toString()),
-                        );
-                      }),
-                      onChanged: (value) {},
-                    ),
-                    DropdownButton<String>(
-                      hint: const Text('Day'),
-                      items: List.generate(32, (index) {
-                        return DropdownMenuItem(
-                          value: (index + 1).toString(),
-                          child: Text((index + 1).toString()),
-                        );
-                      }),
-                      onChanged: (value) {},
-                    ),
-                  ],
-                ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  DropdownButton<String>(
+                    hint: const Text('Year'),
+                    items: List.generate(100, (index) {
+                      return DropdownMenuItem(
+                        value: (2030 + index).toString(),
+                        child: Text((2000 + index).toString()),
+                      );
+                    }),
+                    onChanged: (value) {},
+                  ),
+                  DropdownButton<String>(
+                    hint: const Text('Month'),
+                    items: List.generate(12, (index) {
+                      return DropdownMenuItem(
+                        value: (index + 1).toString(),
+                        child: Text((index + 1).toString()),
+                      );
+                    }),
+                    onChanged: (value) {},
+                  ),
+                  DropdownButton<String>(
+                    hint: const Text('Day'),
+                    items: List.generate(32, (index) {
+                      return DropdownMenuItem(
+                        value: (index + 1).toString(),
+                        child: Text((index + 1).toString()),
+                      );
+                    }),
+                    onChanged: (value) {},
+                  ),
+                ],
               ),
-              const SizedBox(height: 70),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.08),
               const TextField(
                 decoration: InputDecoration(
                   labelText: 'Amount Lend [Rs.]',
@@ -119,7 +119,7 @@ class HomeScreen extends StatelessWidget {
                 ),
                 keyboardType: TextInputType.number,
               ),
-              const SizedBox(height: 30),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
               const TextField(
                 decoration: InputDecoration(
                   labelText: 'Interest Rate (%)',
@@ -127,19 +127,23 @@ class HomeScreen extends StatelessWidget {
                 ),
                 keyboardType: TextInputType.number,
               ),
-              const SizedBox(height: 80),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.1),
               ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 215, 170, 222),
+                  backgroundColor: const Color.fromARGB(255, 64, 3, 75),
                   padding:
                       const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
                 ),
                 child: const Text(
                   'Calculate',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
                 ),
               ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
             ],
           ),
         ),
